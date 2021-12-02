48e1535cbab2bc64ef84a3a5be00ba9787b265b2
Ruslan Kuprieiev <kupruser@gmail.com>
2021-12-02 11:17:22 +0200

    ensure main as default branch

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,6 @@
 import subprocess
 
-subprocess.call(['git', 'init'])
+subprocess.call(['git', 'init', '--initial-branch=main'])
 subprocess.call(['git', 'add', '*'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 
