7b38e245b247bee0a87ced7307f8f72d6ccef267
Ronan Lamy <ronan.lamy@gmail.com>
2022-10-10 14:18:36 +0100

    Update test_dvc.py to match e.g. dvc-s3 and add TestGetUrl

--- dvc-{{cookiecutter.plugin_name}}/dvc_{{cookiecutter.plugin_name}}/tests/test_dvc.py
+++ dvc-{{cookiecutter.plugin_name}}/dvc_{{cookiecutter.plugin_name}}/tests/test_dvc.py
@@ -5,53 +5,58 @@ from dvc.testing.api_tests import (  # noqa, pylint: disable=unused-import
 from dvc.testing.remote_tests import (  # noqa, pylint: disable=unused-import
     TestRemote,
 )
+from dvc.testing.workspace_tests import TestAdd as _TestAdd
 from dvc.testing.workspace_tests import (  # noqa, pylint: disable=unused-import
-    TestAdd,
-    TestImport,
+    TestGetUrl,
+)
+from dvc.testing.workspace_tests import TestImport as _TestImport
+from dvc.testing.workspace_tests import (  # noqa, pylint: disable=unused-import
     TestLsUrl,
 )
 
 
 @pytest.fixture
-def cloud_name():
-    return "{{ cookiecutter.plugin_name }}"
+def cloud(make_cloud):
+    yield make_cloud(typ="{{ cookiecutter.plugin_name }}")
 
 
 @pytest.fixture
 def remote(make_remote, cloud_name):
-    yield make_remote(name="upstream", typ=cloud_name)
+    yield make_remote(name="upstream", typ="{{ cookiecutter.plugin_name }}")
 
 
 @pytest.fixture
 def workspace(make_workspace, cloud_name):
-    yield make_workspace(name="workspace", typ=cloud_name)
+    yield make_workspace(name="workspace", typ="{{ cookiecutter.plugin_name }}")
 
 
-@pytest.fixture
-def stage_md5():
-    raise NotImplementedError
+class TestImport(_TestImport):
+    @pytest.fixture
+    def stage_md5(self):
+        raise NotImplementedError
 
 
-@pytest.fixture
-def is_object_storage():
-    raise NotImplementedError
+    @pytest.fixture
+    def is_object_storage(self):
+        raise NotImplementedError
 
 
-@pytest.fixture
-def dir_md5():
-    raise NotImplementedError
+    @pytest.fixture
+    def dir_md5(self):
+        raise NotImplementedError
 
 
-@pytest.fixture
-def hash_name():
-    raise NotImplementedError
+class TestAdd(_TestAdd):
+    @pytest.fixture
+    def hash_name():
+        raise NotImplementedError
 
 
-@pytest.fixture
-def hash_value():
-    raise NotImplementedError
+    @pytest.fixture
+    def hash_value():
+        raise NotImplementedError
 
 
-@pytest.fixture
-def dir_hash_value(dir_md5):
-    raise NotImplementedError
+    @pytest.fixture
+    def dir_hash_value(dir_md5):
+        raise NotImplementedError
